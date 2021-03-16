cd00edae30819281b019ba755c9a444a169945ab
Giacomo Mirandola <mirgiacomo@gmail.com>
2021-03-16 13:50:57 +0100

    Fix header

--- crud-api/api/headers.php
+++ crud-api/api/headers.php
@@ -1,6 +1,7 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header("Content-type: application/json");
-header("Access-Control-Allow-Methods: POST, GET, PUT, DELETE");
-header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Authorization, X-Requested-With");
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST,GET');
+header('Access-Control-Allow-Headers: *');
+header('Content-Type: application/json');
 ?>
