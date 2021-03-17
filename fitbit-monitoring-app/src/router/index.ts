301d0dc0640446ca3c1e8a463a9aa7cf099a5a89
Giacomo Mirandola <mirgiacomo@gmail.com>
2021-03-17 22:01:04 +0100

    Refactoring, added comments

--- fitbit-monitoring-app/src/router/index.ts
+++ fitbit-monitoring-app/src/router/index.ts
@@ -1,42 +1,40 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
-
 import useFirebaseAuth from "../hooks/firebase-auth";
+
 const state = useFirebaseAuth();
-
-
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    redirect:  '/home'
-  },
-  {
-    path: "/login",
-    name: "login",
-    component: () => import("@/views/LoginPage.vue"),
-  },
-  {
-    path: "/home",
-    name: "home",
-    component: () => import("@/views/HomePage.vue"),
-  },
+	{
+		path: "/",
+		redirect: '/home'
+	},
+	{
+		path: "/login",
+		name: "login",
+		component: () => import("@/views/LoginPage.vue"),
+	},
+	{
+		path: "/home",
+		name: "home",
+		component: () => import("@/views/HomePage.vue"),
+	},
 ];
 
-
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+	history: createWebHistory(process.env.BASE_URL),
+	routes
 })
 
+// If there are no info about user, there is a redirect to login page
 router.beforeEach((to, from, next) => {
-  console.log(state.user.value)
-  if (state.user.value && (to.name === 'login')) {
-    next({name : 'home', replace : true})
-  } else if (!state.user.value && (to.name !== 'login')) {
-    next({name : 'login', replace : true})
-  } else {
-    next();
-  }
+	// console.log(state.user.value)
+	if (state.user.value && (to.name === 'login')) {
+		next({ name: 'home', replace: true })
+	} else if (!state.user.value && (to.name !== 'login')) {
+		next({ name: 'login', replace: true })
+	} else {
+		next();
+	}
 });
 
 export default router
