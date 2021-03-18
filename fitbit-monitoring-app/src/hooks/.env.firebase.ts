4bfbe60a9fe373d67b7cf566929f47903574bad6
Giacomo Mirandola <mirgiacomo@gmail.com>
2021-03-18 08:22:06 +0100

    Upload firebase config structure

---
+++ fitbit-monitoring-app/src/hooks/.env.firebase.ts
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
+  };
+  
+  export default FIREBASE_CONFIG 
