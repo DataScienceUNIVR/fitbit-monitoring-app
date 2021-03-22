dc60da92649eaab9159768139ee25e65f4c0722d
Giacomo Mirandola <mirgiacomo@gmail.com>
2021-03-22 23:14:52 +0100

    Refactoring page, added flip cards

--- fitbit-monitoring-app/src/views/StatisticsPage.vue
+++ fitbit-monitoring-app/src/views/StatisticsPage.vue
@@ -9,87 +9,197 @@
             </ion-toolbar>
         </ion-header>
         <ion-content :fullscreen="true" class="main">
-           
             <ion-header collapse="condense">
                 <!-- <ion-toolbar>
                     <ion-title size="large">{{ $route.params.id }}</ion-title>
                 </ion-toolbar> -->
-                 <ion-item>
-      <ion-label>PERIODO STATISTICHE</ion-label>
-      <ion-select placeholder="SELEZIONA">
-        <ion-select-option value="f">Ultimo giorno</ion-select-option>
-        <ion-select-option value="m">Ultima settimana</ion-select-option>
-        <ion-select-option value="m">Ultimo mese</ion-select-option>
-      </ion-select>
-    </ion-item>
+                <ion-item>
+                    <ion-label>PERIODO STATISTICHE</ion-label>
+                    <ion-select
+                        placeholder="SELEZIONA"
+                        @ionchange="changePeriod"
+                    >
+                        <ion-select-option value="d"
+                            >Ultimo giorno</ion-select-option
+                        >
+                        <ion-select-option value="w"
+                            >Ultima settimana</ion-select-option
+                        >
+                        <ion-select-option value="m"
+                            >Ultimo mese</ion-select-option
+                        >
+                    </ion-select>
+                </ion-item>
             </ion-header>
+
+            <!-- TODO: ciclarli -->
             <ion-row class="statistiche-row">
-                <ion-card class="statistiche-ion-card" style="background-color: #4a0f81;">
-                    <ion-card-header>
-                        <ion-card-subtitle class="statistiche-ion-card-subtitle">MINUTI DI ATTIVITA SEDENTARIA</ion-card-subtitle>
-                        <ion-card-title class="statistiche-ion-card-title">
-                            5241
-                        </ion-card-title>
-                    </ion-card-header>
-                </ion-card>
+                <vue-flip active-click="" width="100%" height="100%">
+                    <template v-slot:front class="front">
+                        <ion-card
+                            class="statistiche-ion-card"
+                            style="background-color: #4a0f81"
+                        >
+                            <ion-card-header>
+                                <ion-card-subtitle
+                                    class="statistiche-ion-card-subtitle"
+                                    >MINUTI DI ATTIVITA'
+                                    SEDENTARIA</ion-card-subtitle
+                                >
+                                <ion-card-title
+                                    class="statistiche-ion-card-title"
+                                >
+                                    25
+                                </ion-card-title>
+                            </ion-card-header>
+                        </ion-card>
+                    </template>
+                    <template v-slot:back class="back">
+                        <ion-card
+                            class="statistiche-ion-card"
+                            style="background-color: #8848b4"
+                        >
+                            <ion-card-header>
+                                <ion-card-subtitle
+                                    class="statistiche-ion-card-subtitle"
+                                    >PASSI DI ATTIVITA
+                                    SEDENTARIA</ion-card-subtitle
+                                >
+                                <ion-card-title
+                                    class="statistiche-ion-card-title"
+                                >
+                                    5241
+                                </ion-card-title>
+                            </ion-card-header>
+                        </ion-card>
+                    </template>
+                </vue-flip>
             </ion-row>
             <ion-row class="statistiche-row">
-                <ion-card class="statistiche-ion-card" style="background-color: #8751b8;">
-                    <ion-card-header>
-                        <ion-card-subtitle class="statistiche-ion-card-subtitle">PASSI DI ATTIVITA SEDENTARIA</ion-card-subtitle>
-                        <ion-card-title class="statistiche-ion-card-title">
-                            1620
-                        </ion-card-title>
-                    </ion-card-header>
-                </ion-card>
+                <vue-flip active-click="" width="100%" height="100%">
+                    <template v-slot:front class="front">
+                        <ion-card
+                            class="statistiche-ion-card"
+                            style="background-color: #7f3c2f"
+                        >
+                            <ion-card-header>
+                                <ion-card-subtitle
+                                    class="statistiche-ion-card-subtitle"
+                                    >MINUTI DI ATTIVITA
+                                    LEGGERA</ion-card-subtitle
+                                >
+                                <ion-card-title
+                                    class="statistiche-ion-card-title"
+                                >
+                                    5241
+                                </ion-card-title>
+                            </ion-card-header>
+                        </ion-card>
+                    </template>
+                    <template v-slot:back class="back">
+                        <ion-card
+                            class="statistiche-ion-card"
+                            style="background-color: #b46354"
+                        >
+                            <ion-card-header>
+                                <ion-card-subtitle
+                                    class="statistiche-ion-card-subtitle"
+                                    >PASSI DI ATTIVITA
+                                    LEGGERA</ion-card-subtitle
+                                >
+                                <ion-card-title
+                                    class="statistiche-ion-card-title"
+                                >
+                                    421
+                                </ion-card-title>
+                            </ion-card-header>
+                        </ion-card>
+                    </template>
+                </vue-flip>
             </ion-row>
             <ion-row class="statistiche-row">
-                <ion-card class="statistiche-ion-card" style="background-color: #2d65c5;">
-                    <ion-card-header>
-                        <ion-card-subtitle class="statistiche-ion-card-subtitle">MINUTI DI ATTIVITA LEGGERA</ion-card-subtitle>
-                        <ion-card-title class="statistiche-ion-card-title">200</ion-card-title>
-                    </ion-card-header>
-                </ion-card>
+                <vue-flip active-click="" width="100%" height="100%">
+                    <template v-slot:front class="front">
+                        <ion-card
+                            class="statistiche-ion-card"
+                            style="background-color: #8d912f"
+                        >
+                            <ion-card-header>
+                                <ion-card-subtitle
+                                    class="statistiche-ion-card-subtitle"
+                                    >MINUTI DI ATTIVITA
+                                    MODERATA</ion-card-subtitle
+                                >
+                                <ion-card-title
+                                    class="statistiche-ion-card-title"
+                                >
+                                    3600
+                                </ion-card-title>
+                            </ion-card-header>
+                        </ion-card>
+                    </template>
+                    <template v-slot:back class="back">
+                        <ion-card
+                            class="statistiche-ion-card"
+                            style="background-color: #a6aa43"
+                        >
+                            <ion-card-header>
+                                <ion-card-subtitle
+                                    class="statistiche-ion-card-subtitle"
+                                    >PASSI DI ATTIVITA
+                                    MODERATA</ion-card-subtitle
+                                >
+                                <ion-card-title
+                                    class="statistiche-ion-card-title"
+                                >
+                                    84
+                                </ion-card-title>
+                            </ion-card-header>
+                        </ion-card>
+                    </template>
+                </vue-flip>
             </ion-row>
             <ion-row class="statistiche-row">
-                <ion-card class="statistiche-ion-card" style="background-color: #5b83c7;">
-                    <ion-card-header>
-                        <ion-card-subtitle class="statistiche-ion-card-subtitle">PASSI DI ATTIVITA LEGGERA</ion-card-subtitle>
-                        <ion-card-title class="statistiche-ion-card-title">2000</ion-card-title>
-                    </ion-card-header>
-                </ion-card>
-            </ion-row>
-            <ion-row class="statistiche-row">
-                <ion-card class="statistiche-ion-card" style="background-color: #626032;">
-                    <ion-card-header>
-                        <ion-card-subtitle class="statistiche-ion-card-subtitle">MINUTI DI ATTIVITA MODERATA</ion-card-subtitle>
-                        <ion-card-title class="statistiche-ion-card-title">1570</ion-card-title>
-                    </ion-card-header>
-                </ion-card>
-            </ion-row>
-            <ion-row class="statistiche-row">
-                <ion-card class="statistiche-ion-card" style="background-color: #858344;">
-                    <ion-card-header>
-                        <ion-card-subtitle class="statistiche-ion-card-subtitle">PASSI DI ATTIVITA MODERATA</ion-card-subtitle>
-                        <ion-card-title class="statistiche-ion-card-title">2100</ion-card-title>
-                    </ion-card-header>
-                </ion-card>
-            </ion-row>
-            <ion-row class="statistiche-row">
-                <ion-card class="statistiche-ion-card" style="background-color: #246c4d;">
-                    <ion-card-header>
-                        <ion-card-subtitle class="statistiche-ion-card-subtitle">MINUTI DI ATTIVITA INTENSA</ion-card-subtitle>
-                        <ion-card-title class="statistiche-ion-card-title">25</ion-card-title>
-                    </ion-card-header>
-                </ion-card>
-            </ion-row>
-            <ion-row class="statistiche-row">
-                <ion-card class="statistiche-ion-card" style="background-color: #37a676;">
-                    <ion-card-header>
-                        <ion-card-subtitle class="statistiche-ion-card-subtitle">PASSI DI ATTIVITA INTENSA</ion-card-subtitle>
-                        <ion-card-title class="statistiche-ion-card-title">3000</ion-card-title>
-                    </ion-card-header>
-                </ion-card>
+                <vue-flip active-click="" width="100%" height="100%">
+                    <template v-slot:front class="front">
+                        <ion-card
+                            class="statistiche-ion-card"
+                            style="background-color: #3092a6"
+                        >
+                            <ion-card-header>
+                                <ion-card-subtitle
+                                    class="statistiche-ion-card-subtitle"
+                                    >MINUTI DI ATTIVITA'
+                                    INTESA</ion-card-subtitle
+                                >
+                                <ion-card-title
+                                    class="statistiche-ion-card-title"
+                                >
+                                    60
+                                </ion-card-title>
+                            </ion-card-header>
+                        </ion-card>
+                    </template>
+                    <template v-slot:back class="back">
+                        <ion-card
+                            class="statistiche-ion-card"
+                            style="background-color: #43acc3"
+                        >
+                            <ion-card-header>
+                                <ion-card-subtitle
+                                    class="statistiche-ion-card-subtitle"
+                                    >PASSI DI ATTIVITA'
+                                    INTESA</ion-card-subtitle
+                                >
+                                <ion-card-title
+                                    class="statistiche-ion-card-title"
+                                >
+                                    9580
+                                </ion-card-title>
+                            </ion-card-header>
+                        </ion-card>
+                    </template>
+                </vue-flip>
             </ion-row>
         </ion-content>
     </ion-page>
@@ -106,17 +216,17 @@ import {
     IonRow,
     IonSelect,
     IonSelectOption,
-    IonCard,
     IonLabel,
+    IonCard,
+    IonCardSubtitle,
     IonCardTitle,
     IonCardHeader,
-    IonCardSubtitle,
     IonItem,
-    // IonIcon,
     IonToolbar,
 } from "@ionic/vue";
-// import { cloudUploadSharp} from "ionicons/icons";
-
+import VueFlip from "vue-flip";
+import { getStatistics } from "../hooks/firebase-crud";
+// let statistiche = "das";
 
 export default {
     name: "Main",
@@ -130,19 +240,28 @@ export default {
         IonRow,
         IonSelect,
         IonSelectOption,
-        IonCard,
         IonLabel,
+        IonCard,
+        IonCardSubtitle,
         IonCardTitle,
         IonCardHeader,
-        IonCardSubtitle,
         IonItem,
-        // IonIcon,
         IonToolbar,
+        "vue-flip": VueFlip,
     },
-    // setup() {
-    //     return { cloudUploadSharp };
-    // },
+    /** Always executed when page is load */
+    ready() {
+        console.log("init");
+    },
+    methods: {
+        changePeriod($event: any) {
+            getStatistics($event.target.value);
+            // location.reload();
+        },
 
-    
+        // setup() {
+        //     return {  };
+        // },
+    },
 };
 </script>
